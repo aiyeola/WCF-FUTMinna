e630a99ba2c9774729465e0d1d25fd6e18013825
aiyeola <aiyeolavictor@gmail.com>
2019-10-17 13:33:00 +0100

    App Configuration

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const fs = require("fs");
+require("./model");
+const app = express();
+const path = require("path");
+const expressHandlebars = require("express-handlebars");
+const bodyparser = require("body-parser");
+const routes = require("./controllers/routes");
+const morgan = require("morgan");
+const multer = require("multer");
+
+const errorHandler = require("errorhandler");
+
+app.use(morgan("dev"));
+app.use(
+  bodyparser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyparser.json());
+
+if ("development" === app.get("env")) {
+  app.use(errorHandler());
+}
+app.use(
+  multer({
+    dest: "./uploads/"
+  }).single("singleInputFileName")
+);
+app.set("views", path.join(__dirname, "/views/"));
+
+app.engine(
+  "hbs",
+  expressHandlebars({
+    extname: "hbs",
+    defaultLayout: "mainLayout",
+    layoutsDir: __dirname + "/views/layouts"
+  })
+);
+app.set("view engine", "hbs");
+
+app.use("/", routes);
+app.listen("3000", () => {
+  console.log("Server started, listening on 3000");
+});
