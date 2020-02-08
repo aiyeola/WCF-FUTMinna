6f1340e4e2f93466be87bdc5c296a9f7801bdd90
Aiyeola Victor <aiyeolavictor@gmail.com>
2020-02-08 12:16:05 +0100

    updated schema

--- models/submitData.js
+++ models/submitData.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 var bioSchema = new mongoose.Schema({
   fullName: {
     type: String,
-    required: "Required"
+    required: "This field is required"
   },
   department: {
+    type: String
+  },
+  email: {
     type: String
   },
   level: {
@@ -22,10 +25,19 @@ var bioSchema = new mongoose.Schema({
   },
   mentors: {
     type: String
+  },
+  campus: {
+    type: String
   }
 });
+// Custom Validation for email
+bioSchema.path("email").validate((val) =>{
+  emailRegex = /\n/ // Still to implement the regex for email 
+  return emailRegex.test(val)
+}, "Invalid email")
 
-const bioForm = mongoose.model("bioForm", bioSchema); 
+
+const bioForm = mongoose.model("bioForm", bioSchema);
 // first parameter is the name of the collection and also the model name
 // second parameter is the schema to be used for the model
 module.exports = bioForm;
