35ac3b2401780f0962f5e8017ce3c319e3c5e3f3
Aiyeola Victor <aiyeolavictor@gmail.com>
2020-02-21 04:06:44 +0100

    App middleware configuration

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ require("dotenv").config();
 require("./models");
 const app = express();
 const path = require("path");
+const Handlebars = require("handlebars");
 const expressHandlebars = require("express-handlebars");
+const {
+  allowInsecurePrototypeAccess
+} = require("@handlebars/allow-prototype-access");
 const bodyparser = require("body-parser");
+const cors = require("cors")
 const indexRoutes = require("./routes/index");
 const adminRoutes = require("./routes/admin");
+const adminRegisterRoutes = require("./routes/admin_register");
 const databaseRoutes = require("./routes/database");
 const morgan = require("morgan");
 const errorHandler = require("errorhandler");
-
 
 app.use(morgan("dev"));
 app.use(
@@ -19,6 +24,11 @@ app.use(
   })
 );
 app.use(bodyparser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 if ("development" === app.get("env")) {
   app.use(errorHandler());
@@ -31,7 +41,8 @@ app.engine(
   expressHandlebars({
     extname: "hbs",
     defaultLayout: "mainLayout",
-    layoutsDir: __dirname + "/views/layouts"
+    layoutsDir: __dirname + "/views/layouts",
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
   })
 );
 
@@ -39,6 +50,7 @@ app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname + "/public")));
 
 app.use("/", indexRoutes);
+app.use("/admin_register", adminRegisterRoutes);
 app.use("/admin", adminRoutes);
 app.use("/database", databaseRoutes);
 
