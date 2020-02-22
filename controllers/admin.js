51ad233cce77627d0db1c8b98eca7aaf38b455a3
Aiyeola Victor <aiyeolavictor@gmail.com>
2020-02-22 04:53:11 +0100

    Updated admin controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,7 +7,10 @@ const {
   sendRefreshToken
 } = require("../utils/tokens");
 
-const render_admin = (req, res) => res.render("admin");
+const render_admin = (req, res) => {
+  res.clearCookie("refreshToken", { path: "/" });
+  res.render("admin");
+};
 
 const render_admin_register = (req, res) => res.render("admin_register");
 
@@ -20,14 +23,13 @@ const register_admin = async (req, res) => {
     if (user.length >= 1) throw new Error("User exists");
     // if user doesn't exist, hash password
     const hashedPassword = await hash(password, 10);
-    console.log(hashedPassword);
 
     // save password to database
     const admin = new Admin({
       username,
       password: hashedPassword
     });
-    admin
+    await admin
       .save()
       .then(res.redirect("/admin"))
       .catch(error => `Error creating Admin record: ${error}`);
@@ -53,19 +55,17 @@ const login_admin = async (req, res) => {
     const valid = await compare(password, user.password);
     if (!valid) throw new Error("password not correct");
     // create a refresh and access token
-    const accessToken = createAccessToken(user.username, user._id);
-    const refreshToken = createRefreshToken(user.username, user._id);
+    const accessToken = createAccessToken(user._id);
+    const refreshToken = createRefreshToken(user._id);
     // put the refresh token in the database
     const update = await Admin.findByIdAndUpdate(
       { _id: user._id },
       { $set: { refreshToken } }
     );
-
-    console.log("updated document", update);
     if (!update)
       throw new Error(" refresh token has not been entered into database");
     // send refreshToken as a cookie and
-    // send access token as a regular response
+    // send access token to  the authorization header
     sendRefreshToken(res, refreshToken);
     sendAccessToken(req, res, accessToken);
   } catch (error) {
@@ -76,15 +76,9 @@ const login_admin = async (req, res) => {
   }
 };
 
-const logout_admin = (_req, res) => {
-  res.clearCookie("refreshToken");
-  res.redirect("/");
-};
-
 module.exports = {
   render_admin,
   login_admin,
-  logout_admin,
   render_admin_register,
   register_admin
 };
